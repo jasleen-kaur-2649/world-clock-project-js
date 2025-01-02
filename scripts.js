1ed4ac073635f4d2720a3701df6ba2d4f3bdb904
Jasleen Kaur <kaurjasleen1728@gmail.com>
2025-01-02 23:01:02 +0530

    Added Current Location Option

--- scripts.js
+++ scripts.js
@@ -52,29 +52,39 @@ setInterval(updateData, 1000);
 function showCity(event) {
   let cityElement = document.querySelector(".cities");
   let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.split("/");
+  // let cityName = cityTimeZone.split("/")
+  let cityName;
 
   // cityElement.innerHTML=cityName
   if (cityTimeZone) {
     if (cityTimeZone === "empty") {
       alert("Please, Select a city..");
     } else {
+      if (cityTimeZone === "current-loc") {
+        cityTimeZone = moment.tz.guess();
+      }
+
       let cityDate = moment().tz(cityTimeZone).format("MMMM Do, YYYY");
       let cityTime = moment()
         .tz(cityTimeZone)
         .format("h:mm:ss [<span>]A[</span>]");
+      if (cityTimeZone.includes("/")) {
+        cityName = cityTimeZone.split("/");
+        cityName = cityName[1];
+      } else {
+        cityName = cityTimeZone;
+      }
 
       //   to update the html...
       cityElement.innerHTML = `
             <div class="city-clock-card">
                 <div class="city-name-date">
-                    <div class="city-name">${cityName[1]}</div>
+                    <div class="city-name">${cityName}</div>
                     <div class="city-date">${cityDate}</div>
                 </div>
                 <div class="city-time">${cityTime}</div>
             </div>
-            <hr class="border-line">
-`;
+            <hr class="border-line">`;
     }
   }
 }
