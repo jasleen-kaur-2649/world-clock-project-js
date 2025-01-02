376fd238fc1fa16aee4dffcb5d67df7528207362
Jasleen Kaur <kaurjasleen1728@gmail.com>
2025-01-02 22:26:07 +0530

    added js to the dropdown menu

--- scripts.js
+++ scripts.js
@@ -1,48 +1,52 @@
 function updateData() {
-  let cityElement = document.querySelector(".cities");
-  if (cityElement.length>0) {
-    let delhiCityElement = document.querySelector("#delhi");
+  let delhiCityElement = document.querySelector("#delhi");
+  if (delhiCityElement) {
+    delhiCityElement.querySelector(".city-name").innerHTML = `Delhi`;
     let delhiCityDate = delhiCityElement.querySelector(".city-date");
     delhiCityDate.innerHTML = `${moment()
       .tz("Asia/Kolkata")
-      .format("MMMM do, YYYY")}`;
+      .format("MMMM Do, YYYY")}`;
     let delhiCityTime = delhiCityElement.querySelector(".city-time");
     delhiCityTime.innerHTML = `${moment()
       .tz("Asia/Kolkata")
       .format("h:mm:ss [<span>]A[</span>]")}`;
 
     let lisbonCityElement = document.querySelector("#lisbon");
+    lisbonCityElement.querySelector(".city-name").innerHTML = `Lisbon`;
     let lisbonCityDate = lisbonCityElement.querySelector(".city-date");
     lisbonCityDate.innerHTML = `${moment()
       .tz("Europe/Lisbon")
-      .format("MMMM do, YYYY")}`;
+      .format("MMMM Do, YYYY")}`;
     let lisbonCityTime = lisbonCityElement.querySelector(".city-time");
     lisbonCityTime.innerHTML = `${moment()
       .tz("Europe/Lisbon")
       .format("h:mm:ss [<span>]A[</span>]")}`;
 
     let canadaCityElement = document.querySelector("#canada");
+    canadaCityElement.querySelector(".city-name").innerHTML = `Canada`;
+
     let canadaCityDate = canadaCityElement.querySelector(".city-date");
     canadaCityDate.innerHTML = `${moment()
       .tz("America/Edmonton")
-      .format("MMMM do, YYYY")}`;
+      .format("MMMM Do, YYYY")}`;
     let canadaCityTime = canadaCityElement.querySelector(".city-time");
     canadaCityTime.innerHTML = `${moment()
       .tz("America/Edmonton")
       .format("h:mm:ss [<span>]A[</span>]")}`;
 
     let spainCityElement = document.querySelector("#spain");
+    spainCityElement.querySelector(".city-name").innerHTML = `Spain`;
+
     let spainCityDate = spainCityElement.querySelector(".city-date");
     spainCityDate.innerHTML = `${moment()
       .tz("America/Puerto_Rico")
-      .format("MMMM do, YYYY")}`;
+      .format("MMMM Do, YYYY")}`;
     let spainCityTime = spainCityElement.querySelector(".city-time");
     spainCityTime.innerHTML = `${moment()
       .tz("America/Puerto_Rico")
       .format("h:mm:ss [<span>]A[</span>]")}`;
   }
 }
-
 setInterval(updateData, 1000);
 
 function showCity(event) {
@@ -51,17 +55,17 @@ function showCity(event) {
   let cityName = cityTimeZone.split("/");
 
   // cityElement.innerHTML=cityName
+  if (cityTimeZone) {
+    if (cityTimeZone === "empty") {
+      alert("Please, Select a city..");
+    } else {
+      let cityDate = moment().tz(cityTimeZone).format("MMMM Do, YYYY");
+      let cityTime = moment()
+        .tz(cityTimeZone)
+        .format("h:mm:ss [<span>]A[</span>]");
 
-  if (cityTimeZone === "empty") {
-    alert("Please, Select a city..");
-  } else {
-    let cityDate = moment().tz(cityTimeZone).format("MMMM do, YYYY");
-    let cityTime = moment()
-      .tz(cityTimeZone)
-      .format("h:mm:ss [<span>]A[</span>]");
-
-    //   to update the html...
-    cityElement.innerHTML = `
+      //   to update the html...
+      cityElement.innerHTML = `
             <div class="city-clock-card">
                 <div class="city-name-date">
                     <div class="city-name">${cityName[1]}</div>
@@ -71,6 +75,7 @@ function showCity(event) {
             </div>
             <hr class="border-line">
 `;
+    }
   }
 }
 
